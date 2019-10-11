ec884e12e4432dcc31e013dc0a9be40ea8191f3e
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2019-10-11 16:18:57 +0900

    Modify to hold multiple data

--- simplot/Simplot.py
+++ simplot/Simplot.py
@@ -1,22 +1,40 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.font_manager
+fontprop = matplotlib.font_manager.FontProperties(fname="/usr/share/fonts/truetype/fonts-japanese-gothic.ttf")
+plt.rcParams['font.family'] = 'IPAPGothic'
 
 class Graph:
-    def __init__(self, style):
+    def __init__(self, title, style):
+        self.title = title
         self.style = style
         self.fig, self.ax = plt.subplots()
-        self.x = None
-        self.y = None
+        self.x = []
+        self.y = []
+        self.xlabel = None
+        self.ylabel = None
         self.legend = None
 
-    def add_data(self, x, y):
-        self.x = x
-        self.y = y
+    def add_data(self, x, y, name=None):
+        if len(x) == len(y):
+            self.x.append(x)
+            self.y.append(y)
+            return 1
+        else:
+            print("ERROR: The data length of x and y is different.")
+            return -1
 
-    def draw(self, output=None):
-        self.ax.plot(self.x, self.y)
-        self.ax.set_title("title")
-        self.ax.set_xlabel("x")
-        self.ax.set_ylabel("y")
+    def draw(self, output=None, show=False):
+        try:
+            for i in range(len(self.x)):
+                self.ax.plot(self.x[i], self.y[i])
+            self.ax.set_title(self.title)
+            self.ax.set_xlabel(self.xlabel)
+            self.ax.set_ylabel(self.ylabel)
 
-        return 1
+            if output: plt.savefig(output)
+            if show: plt.show()
+            return 1
+        except:
+            return -1
--- test/TestSimplot.py
+++ test/TestSimplot.py
@@ -2,9 +2,18 @@ import unittest
 from simplot import Simplot
 
 class TestSimplot(unittest.TestCase):
-    def setUp(self):
-        self.obj = Simplot.Graph("line")
+    def test_differentlen(self):
+        g = Simplot.Graph("TestGraph", "line")
+        self.assertEqual(-1, g.add_data([1,2,3,4,5,6], [10,20,30,40,50], name="Test"))
+    
+    def test_linegraph1(self):
+        g = Simplot.Graph("TestGraph", "line")
+        g.add_data([1,2,3,4,5], [10,20,30,40,50], name="Test")
+        self.assertEqual(1, g.draw())
 
-    def test_one(self):
-        self.obj.add_data([1,2,3,4,5], [10,20,30,40,50])
-        self.assertEqual(1, self.obj.draw())
+    def test_linegraph2(self):
+        g = Simplot.Graph("TestGraph", "line")
+        g.add_data([1,2,3,4,5], [10,20,30,40,50], name="Test1")
+        g.add_data([1,2,3,4,5], [20,30,40,50,60], name="Test2")
+        self.assertEqual(1, g.draw())
+        
